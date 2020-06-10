4a6172bcbb49825af586ebf6c439f7198f3a2752
tonyhew <916390127@qq.com>
2020-06-10 14:13:16 +0800

    用户管理组件

---
+++ admin/src/Pages/UserManage.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import marked from 'marked';
+import '../static/css/AddArticle.css';
+import { Row, Col, Input, Select, Button, DatePicker, message } from 'antd'
+import axios from 'axios'
+import servicePath from '../config/apiUrl'
+
+
+function UserManage (props) {
+  
+  return (
+    <div>userManage</div>
+  )
+}
+
+export default UserManage;
+
+
+
