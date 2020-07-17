06322725854de7524254c6d48a460b2bcbfdcd69
tonyhew <916390127@qq.com>
2020-07-17 15:58:17 +0800

    iconfont的远程项目地址接口

---
+++ blog/config/iconfont.config.js
@@ -0,0 +1,6 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+const IconFont = createFromIconfontCN({
+    scriptUrl: '//at.alicdn.com/t/font_1949929_ypvn7r2ggqs.js',
+});
+
+export default IconFont;
