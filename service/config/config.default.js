5e8383a00fd74801c9443a7a94271c16678bf673
tonyhew <916390127@qq.com>
2020-08-19 17:31:07 +0800

    配置egg-swagger-doc

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -2,16 +2,18 @@
 
 'use strict';
 
+// const { swaggerDocs } = require('./plugin');
+
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+
 module.exports = appInfo => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
   const config = exports = {};
-
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1581092626397_782';
 
@@ -27,15 +29,15 @@ module.exports = appInfo => {
     // database configuration
     client: {
       // host
-      host: 'xx.xx.xx.xx',
+      host: '39.97.67.234',
       // port
       port: '3306',
       // username
-      user: 'xxxxx',
+      user: 'tonyhew',
       // password
-      password: 'xxxxxxxxx',
+      password: 'heweiyi123!',
       // database
-      database: 'xxxxx',
+      database: 'react_blog',
     },
     // load into app, default is open
     app: true,
@@ -64,8 +66,26 @@ module.exports = appInfo => {
     renew: true, // 延长会话有效期
   };
 
+  const swaggerdoc = exports = {
+    dirScanner: './app/controller', // 配置自动扫描的控制器路径。
+    // 接口文档的标题，描述或其它。
+    apiInfo: {
+      title: 'Blog_API', // 接口文档的标题。
+      description: '个人博客接口文档', // 接口文档描述。
+      version: '1.0.0', // 接口文档版本。
+    },
+    schemes: [ 'http', 'https' ], // 配置支持的协议。
+    consumes: [ 'application/json' ], // 指定处理请求的提交内容类型（Content-Type），例如application/json, text/html。
+    produces: [ 'application/json' ], // 指定返回的内容类型，仅当request请求头中的(Accept)类型中包含该指定类型才返回。
+    enableSecurity: false, // 是否启用授权，默认 false（不启用）。
+    // enableValidate: true,    // 是否启用参数校验，默认 true（启用）。
+    routerMap: true, // 是否启用自动生成路由，默认 true (启用)。
+    enable: true, // 默认 true (启用)。
+  };
+
   return {
     ...config,
     ...userConfig,
+    ...swaggerdoc,
   };
 };
