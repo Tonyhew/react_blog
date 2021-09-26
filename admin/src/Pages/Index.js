c80f76141cb9594b5c6768057c873fab3570917e
Tonyhew <916390127@qq.com>
2021-09-26 20:17:05 +0800

    增加公司小红书表单数据查看功能及表单数据新增时短信提醒功能

--- admin/src/Pages/Index.js
+++ admin/src/Pages/Index.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import servicePath from '../config/apiUrl';
 import { Doughnut } from 'react-chartjs-2';
+import { List, Row, Col } from 'antd'
 import '../static/css/Index.css';
 
 function Index(props) {
 	const [typeName, setTypeName] = useState([]);
 	const [count, setCount] = useState([]);
+	const [rblLoading, setRBLLoading] = useState(false);
+	const [redBookList, setRedBookList] = useState([]);
 
 	useEffect(() => {
-		getValueArticle();
+		getValueArticle()
+		getRedBookFormInfo()
+		// eslint-disable-next-line react-hooks/exhaustive-deps 
 	}, []);
 
 	const getValueArticle = () => {
@@ -26,20 +31,51 @@ function Index(props) {
 					props.history.push('/');
 				} else {
 					let result = res.data.countArt
-					for (let item in result) {
-						var dataA = result.reduce((res, v) => {
-							Object.keys(v).forEach(key => {
-								const value = v[key];
-								if (res[key]) res[key].push(value);
-								else res[key] = [value];
-							})
-							return res;
-						}, {});
-						setTypeName(dataA.typeName)
-						setCount(dataA.Count)
-					}
+					var dataA = result.reduce((res, v) => {
+						Object.keys(v).forEach(key => {
+							const value = v[key];
+							if (res[key]) res[key].push(value);
+							else res[key] = [value];
+						})
+						return res;
+					}, {});
+					setTypeName(dataA.typeName)
+					setCount(dataA.Count)
 				}
 
+			}
+		)
+	}
+
+	/**
+	 * 
+	 * 获取小红书报名人员信息
+	 */
+	const getRedBookFormInfo = () => {
+		setRBLLoading(true)
+		axios({
+			method: 'post',
+			url: 'https://backstage.huangzhemin.com/miniapp/XiaoHong/selectRedBookFrom',
+			withCredentials: true,
+			data: {
+				pageIndex: 0,
+				pageSize: 30
+			},
+			header: {
+				'Acess-Control-Allow-Origin': '*',
+			}
+		}).then(
+			res => {
+				if (res.data.data === '没有登录') {
+					localStorage.removeItem('openId');
+					localStorage.removeItem('roleId');
+					props.history.push('/');
+				} else {
+					setRBLLoading(false)
+					let redListData = res.data.data
+					setRedBookList(redListData)
+
+				}
 			}
 		)
 	}
@@ -73,6 +109,80 @@ function Index(props) {
 			<div className='chart_left' style={{ padding: 24, background: '#fff' }}>
 				<p>统计文章</p>
 				<Doughnut data={data} width={600} height={300} />
+			</div>
+
+			<div className="table-redbook" style={{ padding: 24, background: '#fff', marginTop: '20px' }}>
+				<h4>小红书报名表单</h4>
+				<List
+					bordered
+					header={
+						<Row className="list-div">
+							<Col span={4}>
+								<b>ID</b>
+							</Col>
+							<Col span={2}>
+								<b>姓名</b>
+							</Col>
+							<Col span={2}>
+								<b>性别</b>
+							</Col>
+							<Col span={2}>
+								<b>电话</b>
+							</Col>
+							<Col span={4}>
+								<b>邮箱</b>
+							</Col>
+							<Col span={2}>
+								<b>省份</b>
+							</Col>
+							<Col span={2}>
+								<b>城市</b>
+							</Col>
+							<Col span={6}>
+								<b>详细地址</b>
+							</Col>
+						</Row>
+					}
+					loading={rblLoading}
+					dataSource={redBookList}
+					renderItem={
+						(item, index) => {
+							return (
+								<List.Item key={index}>
+
+									<Row className="list-div">
+										<Col span={4}>
+											{item.labelFrom.leadsId}
+										</Col>
+										<Col span={2}>
+											{item.labelFrom.labelName}
+										</Col>
+										<Col span={2}>
+											{item.labelFrom.labelGender}
+										</Col>
+										<Col span={2}>
+											{item.labelFrom.labelPhone}
+										</Col>
+										<Col span={4}>
+											{item.labelFrom.labelMailbox}
+										</Col>
+										<Col span={2}>
+											{item.labelFrom.labelProvince}
+										</Col>
+										<Col span={2}>
+											{item.labelFrom.labelCity}
+										</Col>
+										<Col span={6}>
+											{item.labelFrom.labelAddress}
+										</Col>
+									</Row>
+								</List.Item>
+							)
+						}
+					}
+				>
+
+				</List>
 			</div>
 		</div>
 	)
